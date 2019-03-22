ab31dc2b0aca7e3f6aeeba617a2538321597da42
Hartong, Glenn <Glenn.Hartong@signetjewelers.com>
2019-03-22 11:45:18 -0400

    moved other screen out of app.js

--- App.js
+++ App.js
@@ -1,40 +1,12 @@
 import * as React from 'react'
-import { AsyncStorage, Button, StatusBar, StyleSheet, View } from 'react-native'
 import { Provider, connect } from 'react-redux'
 import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation'
 import { createStore, combineReducers } from 'redux'
 import LoginPage from './src/components/login/LoginPage'
 import HomeScreen from './src/components/HomeScreen'
+import OtherScreen from './src/components/OtherScreen'
 import AuthLoadingScreen from './src/components/login/AuthLoadingScreen'
 import ProfilePage from './src/components/profile/ProfilePage'
-
-class OtherScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Lots of features here',
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
-
-  _signOutAsync = async () => {
-    await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
-  };
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen, Profile: ProfilePage });
 const AuthStack = createStackNavigator({ SignIn: LoginPage });
---
+++ src/components/OtherScreen.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+export default class OtherScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Lots of features here',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>This is some other page.</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
