070c1d30b12dd83edc4b0b0c7b0f5ea65ad0283f
Hartong, Glenn <Glenn.Hartong@signetjewelers.com>
2019-03-22 11:40:28 -0400

    moved homescreen out of app.js

--- App.js
+++ App.js
@@ -1,48 +1,12 @@
 import * as React from 'react'
-import {
-  ActivityIndicator,
-  AsyncStorage,
-  Button,
-  StatusBar,
-  StyleSheet,
-  View,
-} from 'react-native'
+import { AsyncStorage, Button, StatusBar, StyleSheet, View } from 'react-native'
 import { Provider, connect } from 'react-redux'
 import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation'
 import { createStore, combineReducers } from 'redux'
 import LoginPage from './src/components/login/LoginPage'
+import HomeScreen from './src/components/HomeScreen'
 import AuthLoadingScreen from './src/components/login/AuthLoadingScreen'
 import ProfilePage from './src/components/profile/ProfilePage'
-import { facebookService } from './src/library/FacebookService'
-
-class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Welcome to the app!',
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Button title="Show me more of the app" onPress={this._showMoreApp} />
-        <Button title="My FB Profile" onPress={this._showProfile} />
-        {facebookService.makeLogoutButton(() => {  this._signOutAsync  })}
-      </View>
-    );
-  }
-
-  _showProfile = () => {
-    this.props.navigation.navigate('Profile');
-  };
-
-  _showMoreApp = () => {
-    this.props.navigation.navigate('Other');
-  };
-
-  _signOutAsync = async () => {
-    await AsyncStorage.clear();
-    this.props.navigation.navigate('Auth');
-  };
-}
 
 class OtherScreen extends React.Component {
   static navigationOptions = {
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import { AsyncStorage, Button, StyleSheet, View } from 'react-native'
+import { facebookService } from '../library/FacebookService'
+
+
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome to the app!',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button title="Show me more of the app" onPress={this._showMoreApp} />
+                <Button title="My FB Profile" onPress={this._showProfile} />
+                {facebookService.makeLogoutButton(() => { this._signOutAsync })}
+            </View>
+        );
+    }
+
+    _showProfile = () => {
+        this.props.navigation.navigate('Profile');
+    };
+
+    _showMoreApp = () => {
+        this.props.navigation.navigate('Other');
+    };
+
+    _signOutAsync = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
